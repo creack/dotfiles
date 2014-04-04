cd4f28922c0e57f11ffec3c3721854f9b09a54f7
Guillaume J. Charmes <guillaume@charmes.net>
2014-04-04 15:21:41 -0700

    update gitignore

--- dot.config/fish/functions/setup_env.fish
+++ dot.config/fish/functions/setup_env.fish
@@ -2,22 +2,22 @@
 
 function setup_env --description "Setup global environment"
 	# if you call a different shell, this does not happen automatically. WTF?
-	set -x SHELL (which fish)
+	set -gx SHELL (which fish)
 
 	# available since 4.8.0
-	set -x GCC_COLORS 1
+	set -gx GCC_COLORS 1
 
-	set -x HOSTNAME (hostname)
-	set -x PLATFORM (uname -s)
-	set -x ARCH (uname -m)
-	set -x GOBIN $HOME/goroot/bin
-	set -x GOPATH $HOME/go
-	set -x PATH $GOBIN $PATH
+	set -gx HOSTNAME (hostname)
+	set -gx PLATFORM (uname -s)
+	set -gx ARCH (uname -m)
+	set -gx GOBIN $HOME/goroot/bin
+	set -gx GOPATH $HOME/go
+	set -gx PATH $GOBIN $PATH
 
-	set -x PAGER "most"
-	set -x EDITOR "emacsclient -c -t -a=''"
-	set -x WATCH "all"
-	set -x LANG "en_US.UTF-8"
-	set -x GPGKEY CB6E3FF3
+	set -gx PAGER "most"
+	set -gx EDITOR "emacsclient -c -t -a=''"
+	set -gx WATCH "all"
+	set -gx LANG "en_US.UTF-8"
+	set -gx GPGKEY CB6E3FF3
 	#set -x GPG_TTY (tty)
 end
