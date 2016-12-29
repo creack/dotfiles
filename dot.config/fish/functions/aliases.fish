3bbc33f39d4c6fc0e1bf37972011001243335b4b
Guillaume J. Charmes <gcharmes@leaf.ag>
2016-12-29 12:38:22 -0600

    Cleanup
    Signed-off-by: Guillaume J. Charmes <gcharmes@leaf.ag>

--- dot.config/fish/functions/aliases.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-
-alias gocov="sudo -E ~/goroot/bin/gocov test -deps -exclude-goroot . | gocov report"
-alias rm="rm -v"
-alias grep="grep --color=auto -n"
--- dot.config/fish/functions/ntpdate.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-function ntpdate_autoupdate --description "Start a silent ntpdate in background (only in interactive mode)"
-	if not status --is-interactive
-		sudo ntpdate time.apple.com > /dev/null ^ /dev/null &
-	end
-end
