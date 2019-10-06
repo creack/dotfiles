86013481539218fb09e4040ca1a7d16b63dec64a
Guillaume J. Charmes <gcharmes@magicleap.com>
2019-10-06 11:23:52 -0400

    Add back mergetool
    Signed-off-by: Guillaume J. Charmes <gcharmes@magicleap.com>

---
+++ .gitconfig.ml
@@ -0,0 +1,21 @@
+[commit]
+  gpgsign = true
+
+[alias]
+  ci  = commit -s -S
+  b   = branch
+  tag = tag -s
+
+[user]
+  email      = gcharmes@magicleap.com
+  name       = Guillaume J. Charmes
+  signingKey = EBF79BEFA891A12D
+
+[github]
+  user = creack
+
+[url "git@bitbucket.org:"]
+  insteadOf = https://bitbucket.org/
+
+[url "git@github.com:"]
+  insteadOf = https://github.com/
