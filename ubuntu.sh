c0c633fb5bb9b5577315dfeabcd2ea21ca8edc27
Guillaume J. Charmes <guillaume.charmes@immertec.com>
2022-04-26 09:24:10 -0400

    fix ubuntu bootstrap

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+set -e
+
 user=$(whoami)
 if [ ! "${user}" = "root" ]; then
   echo "Granting sudoer nopassword to ${user}" >&2
-  echo "${user} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/90-${user}-root"
+  echo "${user} ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/90-${user}-root"
 fi
 
 curl -fsSL https://tailscale.com/install.sh | sudo sh
