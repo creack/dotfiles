39dd3397f15a5aff83d4b8fd222074f641e4e59f
Guillaume J. Charmes <guillaume.charmes@immertec.com>
2022-04-26 09:22:31 -0400

    add ubuntu bootstrap

--- .zshrc
+++ .zshrc
@@ -209,3 +209,5 @@ function helm kubectl aws {
 }
 
 [ -n "${ZPROF}" ] && zprof
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
---
+++ ubuntu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+user=$(whoami)
+if [ ! "${user}" = "root" ]; then
+  echo "Granting sudoer nopassword to ${user}" >&2
+  echo "${user} ALL=(ALL) NOPASSWD:ALL" > "/etc/sudoers.d/90-${user}-root"
+fi
+
+curl -fsSL https://tailscale.com/install.sh | sudo sh
+
+sudo apt-get install -y tmux most zsh watch htop build-essential mosh unzip python3-pip
+
+if [ ! "${user}" = "root" ]; then
+  pip3 install powerline-status
+fi
+
+make
+
+. "${HOME}/.nvm/nvm.sh"
+nvm install 14
+nvm install 16
